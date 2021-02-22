129bc5a3038f3474f3a6f695902fc41a2eaa496a
Ed Armstrong <ed@frar.ca>
2021-02-22 12:22:45 -0500

    moved source files to source directory

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export default {
+    SCRIPT_DIR : "./tts-scripts",
+    UI_DIR : "./tts-ui",
+    DATA_DIR : "./tts-data",
+    SENT_FILE_DIR : "./tts-data/sent",
+    INCLUDE_DIR : "./include",
+    NAME_FILE : "names.json",
+    LAST_MESSAGE_FILE : "last.json",
+    WRITE_PORT : 39999,
+    READ_PORT : 39998,
+    GLOBAL_FILENAME : "global"
+}
