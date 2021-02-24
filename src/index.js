82e7e956bbb428245b8446e8de49201ee99536b5
Ed Armstrong <ed@frar.ca>
2021-02-24 08:56:54 -0500

    added clean to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
+import FS from 'fs';
 import CLI from './CLI.js';
+import Constants from './constants.js';
+
+if (process.argv.length >= 3){
+    if (process.argv[2] === "clean"){
+        FS.rmdirSync(Constants.SCRIPT_DIR, { recursive: true });
+        FS.rmdirSync(Constants.UI_DIR, { recursive: true });
+        FS.rmdirSync(Constants.DATA_DIR, { recursive: true });
+        process.exit(0);
+    }
+}
+
 new CLI().start();
