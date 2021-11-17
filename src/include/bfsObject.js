138265e719c7725dcd01a15405b307e1e47e2606
Ed Armstrong <ed@frar.ca>
2021-11-17 12:02:01 -0500

    clarified bfsObject.js

--- src/include/bfsObject.js
+++ src/include/bfsObject.js
@@ -1,29 +1,31 @@
 /**
- * Return the first object in a bredth-first-search of child nodes that 
- * has a matching key:value pair.
- * Will return obj if it has the matching key:value pair.
- * @param {*} obj
+ * Return the first object in a breadth-first-search of child objects that 
+ * for a matching key:value pair.
+ * Will return the root object if it has the matching key:value pair.
+ * Will return undefined if no match is found.
+ * The 'test' function accepts the value of key when it is found on an object.
+ * It defaults to simple equivalency.
+ * @param {*} root
  * @param {*} key
  * @param {*} value
- * @param {*} test (key, value) test function defaults to equivalency
+ * @param {*} test (value) test function defaults for equivalency
  * @returns
  */
-function bfsObject(obj, key, value, test) {
+function bfsObject(root, key, value, test) {
     test = test ?? (i => i === value);
 
-    if (typeof obj !== "object") return undefined;
-    const queue = [obj];
+    if (typeof root !== "object") return undefined;
+    const queue = [root];
 
     while (queue.length > 0) {
         const current = queue.shift();
         if (test(current[key])) return current;
 
-        for (const prop of Object.keys(current)) {
-            if (typeof current[prop] == "object") {
+        for (const prop of Object.keys(current))
+            if (typeof current[prop] == "object")
                 queue.push(current[prop]);
-            }
-        }
     }
+    return undefined;
 }
 
 export default bfsObject;
